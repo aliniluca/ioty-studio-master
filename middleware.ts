1ffb2b650ef7fcc5a9b9f255fc11c5e354d945b7
Alin Iluca <alin.iluca95@gmail.com>
2025-06-09 15:32:54 +0300

    Add Edge Runtime specification to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+// Specify that this is an Edge Runtime
+export const runtime = 'edge';
+
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
